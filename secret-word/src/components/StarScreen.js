a03868830973fd86fd8704cb588f870ff0550e01
AndersonGama31 <andersongama31@gmail.com>
2022-06-19 19:23:07 -0300

    Finalização do projeto

--- secret-word/src/components/StarScreen.js
+++ secret-word/src/components/StarScreen.js
@@ -1,14 +1,13 @@
-import React from 'react'
 import "./StarScreen.css";
 
-const StarScreen = ({startGame}) => {
+const GameStart = ({ startGame }) => {
   return (
-    <div className='Start'>
+    <div className="start">
       <h1>Secret Word</h1>
-      <p>Clique no botão abaixo para começar a jogas</p>
-      <button onClick={startGame}>Começar o jogo</button>
+      <p>Clique no botão abaixo para começar a jogar</p>
+      <button onClick={startGame}>Começar jogo</button>
     </div>
-  )
-}
+  );
+};
 
-export default StarScreen
+export default GameStart;
